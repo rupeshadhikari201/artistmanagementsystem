2a30cbdba06183f7d83ac695c77e1196fd4b20e6
Rupesh Kumar Yadav <rupeshaadhikari1@gmail.com>
2026-06-01 22:39:07 +0545

    feat : users crud [user create, edit, list, delete]

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('list', views.user_list, name='users-list'),
+    path('create', views.user_create , name='user-create'),
+    path('edit/<int:user_id>/', views.user_edit , name='user-edit'),
+    path('delete/<int:user_id>/', views.user_delete , name='user-delete'),
     
 ]
