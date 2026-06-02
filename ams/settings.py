871fc6ce91e96e5333fe2460b436e3fe7e185419
rupeshadhikari201 <137412902+rupeshadhikari201@users.noreply.github.com>
2026-06-02 22:03:09 +0545

    fix: configure STATIC_ROOT and WhiteNoise for production

--- ams/settings.py
+++ ams/settings.py
@@ -103,6 +103,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATICFILES_DIRS = [ BASE_DIR / 'static']
 STATIC_URL = 'static/'
+STATIC_ROOT  = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Logging
 LOGGING = {
